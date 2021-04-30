f358f6022d197fccec6580219d6ee34451662f41
Romina Schann <cuqi213@gmail.com>
2021-04-30 23:40:06 +0900

    nav

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+          Hello My Note
+      </div>
+    );
+  }
+} //react automatically is going excuse your render methods in class component
+
+export default Home;
